9c25c9d8975f7ff3c78b623425b2827b22aa25ba
atigyi <tigyi.arpad@gmail.com>
2016-09-08 13:24:10 +0200

    tirtos build settings

--- tirtos/products.mak
+++ tirtos/products.mak
@@ -6,11 +6,11 @@
 # for instructions to download the software required.
 
 # XDC_INSTALL_DIR is the path to XDCtools directory.
-XDC_INSTALL_DIR        =
+XDC_INSTALL_DIR        =/Applications/ti/xdctools_3_31_03_43_core/
 
 # BIOS_INSTALL_DIR is the path to TI-RTOS Kernel (SYS/BIOS) directory. If you
 # have installed TI-RTOS, it is located in the products/bios_* path.
-BIOS_INSTALL_DIR       =
+BIOS_INSTALL_DIR       =/Applications/ti/tirtos_cc32xx_2_16_01_14/products/bios_6_45_02_31/
 
 # NDK_INSTALL_DIR is the path to TI-RTOS NDK directory. If you have
 # installed TI-RTOS, it is located in the products/ndk_* path.
@@ -20,11 +20,13 @@ NDK_INSTALL_DIR        =
 # installed TI-RTOS, it is located in the products/TivaWare_* path.
 TIVAWARE_INSTALL_DIR   =
 
+export XDCTOOLS_JAVA_HOME=/Applications/ti/ccsv6/eclipse/jre/Contents/Home/
+
 # Define the code generation tools path for TI, IAR and GCC ARM compilers.
 # If you have installed Code Composer Studio, the TI and GCC compiler are
 # located in the ccsv*/tools/compiler/* path.
 #
 # Leave assignment empty to disable any toolchain.
-ti.targets.arm.elf.M4F =
+ti.targets.arm.elf.M4F =/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS
 iar.targets.arm.M4F    =
 gnu.targets.arm.M4F    =
--- wolfssl/wolfcrypt/settings.h
+++ wolfssl/wolfcrypt/settings.h
@@ -548,6 +548,63 @@ static char *fgets(char *buff, int sz, FILE *fp)
 #endif
 
 #ifdef WOLFSSL_TIRTOS
+
+    #define SINGLE_THREADED
+
+    #define NO_OCSP
+    #define NO_DES3
+    #define NO_OLD_TLS
+    #define NO_PSK
+    #define NO_PWDBASED
+    #define CUSTOM_RAND_GENERATE xively_ssl_rand_generate
+    #define CUSTOM_XTIME xively_ssl_time
+    #define HAVE_SNI
+
+    // #define HAVE_OCSP
+    // #define HAVE_CERTIFICATE_STATUS_REQUEST
+
+    #define HAVE_ANON
+    #define SMALL_SESSION_CACHE
+    #define NO_CLIENT_CACHE
+    #define WOLFSSL_SMALL_STACK
+    #define WOLFSSL_USER_IO
+    #define TARGET_IS_CC3200
+
+    #define NO_RABBIT
+    #define NO_MD4
+    #define NO_RC4
+    #define NO_DH
+    #define NO_DSA
+    #define NO_SHA
+    #define NO_HC128
+
+    #define SIZEOF_LONG_LONG 8
+    #define NO_WRITEV
+    #define NO_WOLFSSL_DIR
+    #define USE_FAST_MATH
+    #define TFM_TIMING_RESISTANT
+    #define NO_DEV_RANDOM
+    #define NO_FILESYSTEM
+    #define USE_CERT_BUFFERS_2048
+    #define NO_ERROR_STRINGS
+    #define USER_TIME
+    #define HAVE_ECC
+    // #define HAVE_ALPN
+    #define HAVE_TLS_EXTENSIONS
+    #define HAVE_AESGCM
+    // #define HAVE_SUPPORTED_CURVES
+    #define ALT_ECC_SIZE
+
+    #ifdef __IAR_SYSTEMS_ICC__
+        #pragma diag_suppress=Pa089
+    #elif !defined(__GNUC__)
+        /* Suppress the sslpro warning */
+        #pragma diag_suppress=11
+    #endif
+
+#endif
+
+#ifdef WOLFSSL_TIRTOS_off
     #define SIZEOF_LONG_LONG 8
     #define NO_WRITEV
     #define NO_WOLFSSL_DIR
